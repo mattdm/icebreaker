98946d33eafbdad04c59dafa1d4d7f765d891c6f
Matthew Miller <mattdm@mattdm.org>
2021-03-01 16:57:02 -0500

    no longer needed since Fedora Linux includes these

--- build-mingw32-sdllibmixer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export CFLAGS="-s"
-export CXXFLAGS="-s"
-cross-configure --disable-music-midi --disable-music-timidity-midi --disable-music-native-midi --disable-music-native-midi-gpl --disable-music-mod --disable-music-mp3 --disable-music-ogg
-cross-make
-find . -name '*.dll' -ls
-echo now do \"cross-make install\".
